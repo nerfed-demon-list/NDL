698d7c4b42eb28c078dda2066d75968488c19a7a
nerfed-demon-list <termlesshawk@op.pl>
2025-07-14 22:12:58 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,8 +2,9 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
+// Vue Router route definitions
 export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
+  { path: '/', component: List },
+  { path: '/leaderboard', component: Leaderboard },
+  { path: '/roulette', component: Roulette },
 ];
