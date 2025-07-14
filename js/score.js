e1f54d9b596967c834498518c4f074fc2b19e5c1
nerfed-demon-list <termlesshawk@op.pl>
2025-07-14 22:13:44 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -18,37 +18,24 @@ export function score(rank, percent, minPercent) {
         return 0;
     }
 
-    // Old formula
-    /*
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let baseScore = (-24.9975 * Math.pow(rank - 1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
-    score = Math.max(0, score);
+    baseScore = Math.max(0, baseScore);
 
-    if (percent != 100) {
-        return round(score - score / 3);
+    if (percent !== 100) {
+        return round(baseScore - baseScore / 3);
     }
 
-    return Math.max(round(score), 0);
+    return Math.max(round(baseScore), 0);
 }
 
+/**
+ * Rounds a number to the given scale (decimal digits)
+ * @param {Number} num
+ * @returns {Number}
+ */
 export function round(num) {
-    if (!('' + num).includes('e')) {
-        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
-    } else {
-        var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
-    }
+    return Number(num.toFixed(scale));
 }
