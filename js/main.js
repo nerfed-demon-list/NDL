e361eeed82da8e549bfa9ddc9fc1adb144e87816
nerfed-demon-list <termlesshawk@op.pl>
2025-07-14 22:11:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 import routes from './routes.js';
 
+// Create a reactive store for theme toggling
 export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) || false,
-    toggleDark() {
-        this.dark = !this.dark;
-        localStorage.setItem('dark', JSON.stringify(this.dark));
-    },
+  dark: JSON.parse(localStorage.getItem('dark')) || false,
+  toggleDark() {
+    this.dark = !this.dark;
+    localStorage.setItem('dark', JSON.stringify(this.dark));
+  },
 });
 
 const app = Vue.createApp({
-    data: () => ({ store }),
+  setup() {
+    return { store };
+  },
 });
+
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-    routes,
+  history: VueRouter.createWebHashHistory(),
+  routes,
 });
 
 app.use(router);
-
 app.mount('#app');
